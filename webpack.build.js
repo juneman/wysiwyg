eb5bff34cdac05a54d3b1f02b2e1ab028ca80b59
Guru Mahendran <me@gurubavan.com>
2017-07-03 01:28:39 -0400

    missing semicolon

--- webpack.build.js
+++ webpack.build.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 const PROJ_ROOT = __dirname,
       SCRIPTS_DIR = path.join(PROJ_ROOT, 'src'),
-      BUILD_DIR = path.join(PROJ_ROOT, (process.env.AWS_BUCKET) ? `lib/${process.env.AWS_BUCKET}` : 'lib'),
+      BUILD_DIR = path.join(PROJ_ROOT, (process.env.AWS_BUCKET) ? `lib/${process.env.AWS_BUCKET}` : 'lib');
 
 module.exports = {
   entry: { index: `${SCRIPTS_DIR}/index` },
