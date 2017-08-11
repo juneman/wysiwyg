c6c33c647ad22bb4d7b0cb3413a60ba81fce133d
helenphan0 <helen.phan@gmail.com>
2017-08-11 14:31:33 -0400

    Add new icons for component categories

---
+++ src/icons/RightButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import IconButton from './IconButton';
+
+export default class AdvancedStyling extends React.PureComponent {
+  render() {
+    return (
+      <IconButton
+        title="menu"
+        pathNode={
+          <path d="M4.5 7q0 0.203-0.148 0.352l-3.5 3.5q-0.148 0.148-0.352 0.148t-0.352-0.148-0.148-0.352v-7q0-0.203 0.148-0.352t0.352-0.148 0.352 0.148l3.5 3.5q0.148 0.148 0.148 0.352z"></path>
+        }
+        viewBox="0 0 14 14"
+        {...this.props}
+      />
+    );
+  }
+}
