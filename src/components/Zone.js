09bdcaf25b2ee3b244dc0698a9c135db1a5421a9
helenphan0 <helen.phan@gmail.com>
2017-09-20 14:48:19 -0400

    Fix width for zone hover state

--- src/components/Zone.js
+++ src/components/Zone.js
@@ -87,7 +87,7 @@ export class Zone extends React.Component {
       display: 'inline-block',
       margin: `0 -${ props.basePadding }px`,
       padding: `0 ${ props.basePadding }px`,
-      width: `calc(100% + ${ props.basePadding * 2 }px - 2px)`,
+      width: `calc(100% + ${ props.basePadding * 2 }px - 1px)`,
       transition: 'background-color 0.15s ease-out, box-shadow 0.15s ease-out, outline-color 0.15s ease-out'
     };
 
