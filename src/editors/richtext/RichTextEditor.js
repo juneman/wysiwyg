dd06489873b3307bd30cd6ccde2ac44d60b2060c
helenphan0 <helen.phan@gmail.com>
2017-09-22 15:09:02 -0400

    Added comments

--- src/editors/richtext/RichTextEditor.js
+++ src/editors/richtext/RichTextEditor.js
@@ -34,17 +34,18 @@ export default class RichTextEditor extends React.Component {
 
     if (isEditing) {
 
+      // If mouseUp happened over a Toolbar element,
+      // don't reset editor focus.
       const hasToolbarElement = document.getElementById('appcues-host').shadowRoot.firstChild.getElementsByClassName('resolved')[0].shadowRoot.firstChild.querySelectorAll('[name="EditorWrapperEditingToolbar"]').length;
 
       if (hasToolbarElement) {
-        return
+        return;
       }
-
-      const editorState = localState.get('editorState');
 
       if (this.editor && !ReactDOM.findDOMNode(this.wrapper).contains(e.path[0])){
           this.editor.blur();
-          const newEditorState = EditorState.forceSelection(editorState, getResetSelection(editorState))
+          const editorState = localState.get('editorState');
+          const newEditorState = EditorState.forceSelection(editorState, getResetSelection(editorState));
           this.handleEditorStateChange(newEditorState);
       }
 
