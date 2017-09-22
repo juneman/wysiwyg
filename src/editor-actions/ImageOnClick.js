eaba3a005e3c6f039bcb7aa6e69ae1571f06486e
helenphan0 <helen.phan@gmail.com>
2017-09-22 08:48:00 -0400

    Handle image onclick actions, Save to persisted state

--- src/editor-actions/ImageOnClick.js
+++ src/editor-actions/ImageOnClick.js
@@ -2,46 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
 
-import { getButtonProps, inputStyle, checkboxStyle, labelStyle, secondaryMenuTitleStyle, fieldGroupStyle, tabStyle, selectedTabStyle, selectMenuStyle, shortInputStyle, buttonNavTypeWrapperStyle, buttonNavTypeMenuStyle, dropdownStyle } from '../helpers/styles/editor';
-import { BUTTON_ACTION_TYPES, BUTTON_ACTION_TYPES_LIST, BUTTON_ACTIONS_WITH_DATA_STEP_ATTRS } from '../helpers/constants';
+import { getButtonProps, inputStyle, checkboxStyle, labelStyle, secondaryMenuTitleStyle, fieldGroupStyle, tabStyle, selectedTabStyle, buttonNavTypeWrapperStyle, buttonNavTypeMenuStyle, dropdownStyle } from '../helpers/styles/editor';
+import {IMG_ACTION_TYPES } from '../helpers/constants';
 import Menu from '../components/Menu';
 import Button from '../components/Button';
 import DropDownMenu from '../components/DropDownMenu';
 
-import ActionButton from '../icons/ActionButton';
+import LinkButton from '../icons/LinkButton';
 
-export default class ImageOnClickAction extends React.Component {
+export default class ImageOnClick extends React.Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
+      isMenuOpen: props.isActive || false,
+      actionType: IMG_ACTION_TYPES.URL,
       href: props.href || '',
       isNewWindow: props.isNewWindow || false,
-      isMenuOpen: props.isActive || false,
-      buttonActionType: BUTTON_ACTION_TYPES.NEXT_PAGE,
-      stepIndex: 0,
       flowId: ''
     };
   }
 
   componentWillReceiveProps(nextProps){
     const update = {};
+    if (nextProps.isActive !== this.props.isActive) {
+      update.isMenuOpen = nextProps.isActive;
+    };
     if (nextProps.href !== this.props.href) {
       update.href = nextProps.href;
     };
     if (nextProps.isNewWindow !== this.props.isNewWindow) {
       update.isNewWindow = nextProps.isNewWindow;
     };
-    if (nextProps.isActive !== this.props.isActive) {
-      update.isMenuOpen = nextProps.isActive;
-    };
 
     const { persistedState } = nextProps;
     const href = persistedState.get('href');
     const isNewWindow = persistedState.get('isNewWindow');
-    const stepIndex = persistedState.get('stepIndex');
-    const buttonActionType = persistedState.get('buttonActionType');
     const flowId = persistedState.get('flowId')
 
     if (href !== undefined) {
@@ -52,16 +49,9 @@ export default class ImageOnClickAction extends React.Component {
       update.isNewWindow = isNewWindow;
     }
 
-    if (buttonActionType !== undefined) {
-      update.buttonActionType = buttonActionType;
-    }
-
-    if (stepIndex !== undefined) {
-      update.stepIndex = stepIndex;
-    }
-
     if (flowId) {
-      update.flowId = flowId
+      update.flowId = flowId;
+      update.actionType = IMG_ACTION_TYPES.APPCUES;
     }
 
     if (Object.keys(update).length) {
@@ -70,8 +60,8 @@ export default class ImageOnClickAction extends React.Component {
   }
 
   render() {
-    const { persistedState, isActive, hasRoomToRenderBelow, numPages, isFirst, isLast } = this.props;
-    const { href, isNewWindow, isMenuOpen, buttonActionType, stepIndex, flowId } = this.state;
+    const { persistedState, isActive, hasRoomToRenderBelow } = this.props;
+    const { isMenuOpen, actionType, flowId, href, isNewWindow } = this.state;
 
     const buttonProps = getButtonProps(isActive);
 
@@ -86,60 +76,50 @@ export default class ImageOnClickAction extends React.Component {
     };
 
     const titleStyles = secondaryMenuTitleStyle;
-    const hasMoreThanOneStep = numPages > 1;
+    const row = {
+      marginTop: 10,
+      display: 'flex'
+    };
 
     const dropdownNodes = isActive ? (
       <Menu style={dropdownStyles}>
-
-        <div style={{...titleStyles, marginBottom: '5px'}}>Button Actions</div>
-
-          <div style={buttonNavTypeMenuStyle}>
-            <DropDownMenu
-              className="form-control"
-              label="On click"
-              unsearchable
-              selectedValue={ buttonActionType }
-              options={ BUTTON_ACTION_TYPES_LIST }
-              onSelect={ (value) => this.handleAction(value) }/>
+          <div style={buttonNavTypeWrapperStyle}>
+            <div
+              onClick={() => this.selectOnClickAction(IMG_ACTION_TYPES.URL)}
+              style={{  ...tabStyle, ...(actionType === IMG_ACTION_TYPES.URL && selectedTabStyle), flexGrow: 1,  }}>
+                Go to URL
+            </div>
+            <div
+              onClick={() => this.selectOnClickAction(IMG_ACTION_TYPES.APPCUES)}
+              style={{ ...tabStyle, ...(actionType === IMG_ACTION_TYPES.APPCUES && selectedTabStyle),flexGrow: 1 }}>
+                Trigger Flow
+            </div>
           </div>
-
-          { buttonActionType === BUTTON_ACTION_TYPES.URL &&
-            <div style={buttonNavTypeMenuStyle}>
-              <div style={ fieldGroupStyle }>
-                <label style={ labelStyle }>Link to URL</label>
-                <input autoFocus type="text" style={ inputStyle } value={href} onClickCapture={this.handleClick} onChange={(e) => this.handleHref(e)} />
+          <div style={buttonNavTypeMenuStyle}>
+            { actionType === IMG_ACTION_TYPES.URL &&
+                <div>
+                  <div style={{ ...row, flexDirection: 'column' }}>
+                    <label style={ labelStyle }>URL</label>
+                    <input autoFocus type="text" style={ inputStyle } value={href} onClickCapture={this.handleClick} onChange={(e) => this.handleHref(e)} />
+                  </div>
+                  <div style={{ ...row, alignItems: 'center' }}>
+                    <input id="link-checkbox" type="checkbox" style={ checkboxStyle } checked={isNewWindow} onChange={(e) => this.handleIsNewWindow(e)} />
+                    <label htmlFor="link-checkbox">Open In New Window</label>
+                  </div>
+                </div>
+            }
+            { actionType === IMG_ACTION_TYPES.APPCUES &&
+              <div>
+                <div style={ fieldGroupStyle }>
+                  <label style={ labelStyle }>Flow ID</label>
+                  <input type="text" value={ flowId } style={ inputStyle } onChange={(e) => this.handleAppcuesShow(e)}/>
+                </div>
+                <p style={{marginTop: '10px', lineHeight: '16px'}}>Enter the Flow ID of a published flow to trigger it from this image.
+                </p>
               </div>
-              <div style={{ marginTop: 5 }}>
-                <input id="link-checkbox" type="checkbox" style={checkboxStyle} checked={isNewWindow} onChange={(e) => this.handleIsNewWindow(e)} />
-                <label htmlFor="link-checkbox">Open In New Window</label>
-              </div>
-            </div>
-          }
-          { buttonActionType === BUTTON_ACTION_TYPES.CUSTOM_PAGE &&
-            <div style={buttonNavTypeMenuStyle}>
-              <div style={ fieldGroupStyle }>
-                <label style={ labelStyle }>Step number</label>
-                <input type="number" min={1} max={numPages} value={ hasMoreThanOneStep ? stepIndex + 1 : 1} disabled={!hasMoreThanOneStep} style={ shortInputStyle } onChange={(e) => this.handleStepIndex(e)}/>
-              </div>
-              <p style={{marginTop: '10px', lineHeight: '16px'}}>
-                { 
-                  `This group contains ${ numPages === 1 ? 'only' : '' } ${ numPages || 'an unknown number of' } step${ hasMoreThanOneStep ? 's' : '' }. ${ hasMoreThanOneStep ? 'Set a number to this button to direct users to that specific step.' : '' }`
-                }
-              </p>
-            </div>
-          }
-
-          { buttonActionType === BUTTON_ACTION_TYPES.APPCUES &&
-            <div style={buttonNavTypeMenuStyle}>
-              <div style={ fieldGroupStyle }>
-                <label style={ labelStyle }>Flow ID</label>
-                <input type="text" value={ flowId } style={ inputStyle } onChange={(e) => this.handleAppcuesShow(e)}/>
-              </div>
-              <p style={{marginTop: '10px', lineHeight: '16px'}}>Enter the Flow ID of a published flow to trigger it from this button.
-              </p>
-            </div>
-          }
-
+            }
+          </div>
+          
         <div style={{textAlign: 'right', marginTop: '5px'}}>
           <Button className="btn" onClick={(e) => this.saveAction(e)}>Save</Button>
         </div>
@@ -149,7 +129,7 @@ export default class ImageOnClickAction extends React.Component {
 
     return (
       <div>
-        <ActionButton onClick={() => this.toggleDropdown()} {...buttonProps} />
+        <LinkButton onClick={() => this.toggleDropdown()} {...buttonProps} />
         { dropdownNodes }
       </div>
     );
@@ -167,6 +147,10 @@ export default class ImageOnClickAction extends React.Component {
     } else {
       onToggleActive(!isActive);
     }
+  }
+
+  selectOnClickAction(actionType) {
+    this.setState({ actionType });
   }
 
   handleHref(e) {
@@ -187,80 +171,37 @@ export default class ImageOnClickAction extends React.Component {
     });
   }
 
-  handleStepIndex(e) {
-    const value = e.target.value;
-
-    this.setState({
-      stepIndex: value - 1
-    });
-  }
-
-  handleAction(value) {
-
-    this.setState({
-      buttonActionType: value
-    });
-  }
-
   handleAppcuesShow(e) {
     const value = e.target.value;
-
     this.setState({
-      buttonActionType: BUTTON_ACTION_TYPES.APPCUES,
       flowId: value
     });
-
   }
 
-  getPersistedStateByButtonActionType(buttonActionType, persistedState, state={}) {
-    const { stepIndex } = this.state;
+  getNewState(currentActionType) {
+    const { persistedState } = this.props;
+    const { flowId, href, isNewWindow } = this.state;
 
-    if (BUTTON_ACTIONS_WITH_DATA_STEP_ATTRS.includes(buttonActionType)) {
+    if (currentActionType === IMG_ACTION_TYPES.URL) {
       return persistedState
-        .set('buttonActionType', buttonActionType)
-        .delete('href')
+        .set('href', href)
+        .set('isNewWindow', isNewWindow)
         .delete('flowId')
-        .delete('stepIndex')
-        .delete('isNewWindow');
     }
-
-    if (buttonActionType == BUTTON_ACTION_TYPES.CUSTOM_PAGE && stepIndex !== undefined) {
+    if (currentActionType === IMG_ACTION_TYPES.APPCUES) {
       return persistedState
-        .set('buttonActionType', buttonActionType)
-        .set('stepIndex', stepIndex)
-        .delete('href')
-        .delete('flowId')
-        .delete('isNewWindow');
-    }
-
-    if (buttonActionType == BUTTON_ACTION_TYPES.URL) {
-      return persistedState
-        .set('buttonActionType', buttonActionType)
-        .set('href', state.hrefWithProtocol)
-        .set('isNewWindow', state.isNewWindow)
-        .delete('stepIndex')
-        .delete('flowId');
-    }
-
-    if (buttonActionType == BUTTON_ACTION_TYPES.APPCUES) {
-      return persistedState
-        .set('buttonActionType', buttonActionType)
-        .set('flowId', state.flowId)
+        .set('flowId', flowId)
         .delete('href')
         .delete('isNewWindow')
-        .delete('stepIndex');
     }
 
-    return persistedState;
   }
 
   saveAction() {
     const { localState, persistedState, onChange, onToggleActive } = this.props;
-    const { isMenuOpen, isNewWindow, href, buttonActionType, flowId } = this.state;
+    const { actionType, isMenuOpen, flowId, href, isNewWindow } = this.state;
 
-    const hrefWithProtocol = (href.includes('://') || href.includes('//')) ? href : '//' + href;
-
-    const newPersistedState = this.getPersistedStateByButtonActionType(buttonActionType, persistedState, { hrefWithProtocol, isNewWindow, flowId });
+    const newPersistedState = this.getNewState(actionType)
 
     this.setState({
       isMenuOpen: !isMenuOpen
@@ -276,10 +217,10 @@ export default class ImageOnClickAction extends React.Component {
 
 }
 
-ImageOnClickAction.propTypes = {
+ImageOnClick.propTypes = {
   href: PropTypes.string,
   isNewWindow: PropTypes.bool,
-  buttonActionType: PropTypes.string,
+  flowId: PropTypes.string,
   localState: PropTypes.instanceOf(Map).isRequired,
   persistedState: PropTypes.instanceOf(Map).isRequired,
   onChange: PropTypes.func.isRequired,
