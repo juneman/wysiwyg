1643c566c80be77e72afb4ffc58874983595748b
helenphan0 <helen.phan@gmail.com>
2017-09-29 09:18:44 -0400

    Load preset images with cloudinary autoformat browser detection

--- src/helpers/presets.js
+++ src/helpers/presets.js
@@ -66,43 +66,43 @@ export const presetGallery = {
     'image': [
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581442/appcues-dev/tk9kgigdjpvwfbstigqy.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/c_lfill,q_90,w_800/v1496931466/14981/qdorltjp5lsiv5rdb0ur.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581442/appcues-dev/tk9kgigdjpvwfbstigqy.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/c_lfill,q_90,w_800,f_auto/v1496931466/14981/qdorltjp5lsiv5rdb0ur.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581529/appcues-dev/x4uc1yzmd0dhh6ljcste.png',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097446/appcues-dev/qfqtt1rzcfuniw1u5kvv.png'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581529/appcues-dev/x4uc1yzmd0dhh6ljcste.png',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097446/appcues-dev/qfqtt1rzcfuniw1u5kvv.png'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500580947/appcues-dev/zgpyya23z1muz25ohgza.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097510/appcues-dev/lwrxgeujaakvarxdqxsr.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500580947/appcues-dev/zgpyya23z1muz25ohgza.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097510/appcues-dev/lwrxgeujaakvarxdqxsr.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581353/appcues-dev/csxijdtxrxqkip3cscqv.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097550/appcues-dev/bplefflfgzq8qzc5mjq5.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581353/appcues-dev/csxijdtxrxqkip3cscqv.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097550/appcues-dev/bplefflfgzq8qzc5mjq5.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581210/appcues-dev/vsudv3f31erwrtrh3kxq.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097598/appcues-dev/ho7i9ttcmnf1ezq7lofy.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581210/appcues-dev/vsudv3f31erwrtrh3kxq.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097598/appcues-dev/ho7i9ttcmnf1ezq7lofy.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581160/appcues-dev/ruj5ysoiqmuqkxwllmix.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097625/appcues-dev/bew13cceknmxg1uuhvy9.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581160/appcues-dev/ruj5ysoiqmuqkxwllmix.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097625/appcues-dev/bew13cceknmxg1uuhvy9.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581076/appcues-dev/byxg9ket5bsid4piamuv.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097649/appcues-dev/ipxsa7xctutqma2gikrz.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581076/appcues-dev/byxg9ket5bsid4piamuv.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097649/appcues-dev/ipxsa7xctutqma2gikrz.jpg'
       },
       {
         type: 'url',
-        src: '//res.cloudinary.com/appcues-dev/image/upload/v1500581019/appcues-dev/fyye2tcrtoyyxrl2lupm.jpg',
-        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/v1501097667/appcues-dev/io9imwcj5ymmofy2yesy.jpg'
+        src: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1500581019/appcues-dev/fyye2tcrtoyyxrl2lupm.jpg',
+        fullResSrc: '//res.cloudinary.com/appcues-dev/image/upload/f_auto/v1501097667/appcues-dev/io9imwcj5ymmofy2yesy.jpg'
       }
     ]
   };
