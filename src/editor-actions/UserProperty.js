5cf9328f6eec53239215602a8d268a99ada65fe9
Mike Thomson <michaelgt04@gmail.com>
2018-03-15 14:10:09 -0400

    Design tweaks

--- src/editor-actions/UserProperty.js
+++ src/editor-actions/UserProperty.js
@@ -75,7 +75,7 @@ export default class UserProperty extends React.Component {
       return userProperty.toJS();
     })].map((userProperty) => ({ label: userProperty.name, ...userProperty }));
 
-    const valueOptions = !selectedProperty ? null : [ { label: "No fallback", value: null }, ...userPropertiesDropdown.find((userProperty) => userProperty.value == selectedProperty).options.map((option) => ({ label: option.name, value: option.name })), { label: 'Custom property', value: 'custom' }];
+    const valueOptions = !selectedProperty ? null : [ { label: "No fallback", value: null }, ...userPropertiesDropdown.find((userProperty) => userProperty.value == selectedProperty).options.map((option) => ({ label: option.name, value: option.name })), { label: 'Custom text...', value: 'custom' }];
 
     const dropdownNodes = isActive ? (
       <Menu style={dropdownStyles}>
@@ -99,7 +99,7 @@ export default class UserProperty extends React.Component {
               inputStyles={propertyInputStyle}
               handleChange={this.setSelectedValue}
               stateProperty="customValue"
-              placeholder="Type custom property here..."
+              placeholder="Type custom text..."
               value={customValue}
             />
             }
