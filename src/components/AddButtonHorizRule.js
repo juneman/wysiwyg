2239c5f93f6c7dddc28f7555d35edd75cede7f55
Aron Thomas <aron.c.thomas@gmail.com>
2018-04-12 11:29:43 -0400

    Add Row Button: Re-center and add animation

--- src/components/AddButtonHorizRule.js
+++ src/components/AddButtonHorizRule.js
@@ -7,6 +7,8 @@ import AddButtonContainer from './AddButtonContainer';
 // Distance threshold before button repositions to
 // opposite side
 const BUTTON_POSITION_ALLOWANCE = 100;
+const ADD_ROW_EXPANDED = 32;
+const ADD_ROW_COLLAPSED = 2;
 
 /**
  * A React component renders an Add button to
@@ -48,54 +50,103 @@ export default class AddButtonHorizRule extends React.Component {
     const { onSelectEditorType, internalAllowedEditorTypes, isHoveringOverContainer } = this.props;
     const { showEditorSelector, isHoveringOverAddButton, hasRoomToRenderOnRight } = this.state;
 
-    const hrStyle = {
-      height: 1,
-      border: 0,
-      position: 'relative',
-      top: 15,
+    let hrStyle = {
+      boxSizing: 'border-box',
+      height: ADD_ROW_COLLAPSED,
+      position: 'absolute',
+      top: 8,
       width: '100%',
       zIndex: 2,
       padding: 0,
       margin: 0,
       pointerEvents: 'none',
-      background: '#00b850',
-      transition: 'transform 0.15s ease-out, opacity 0.15s ease-out',
-      opacity: `${(isHoveringOverAddButton) ? .3 : .25}`,
-      transform: `scale(1, ${(isHoveringOverAddButton) ? 3 : 1})`
+      background: '#d4fee6',
+      border: '1px solid #00b850',
+      opacity: 0,
+      transition: 'height 0.15s ease-out, transform 0s, opacity 0.3s ease-out 0.1s',
+      transformOrigin: 'center 25% 0px',
+      backfaceVisibility: 'hidden',
+      transform: `translateY(-50%) translateZ(0)`
     };
 
-    const addButtonStyle = {
+    let addButtonStyle = {
       position: 'absolute',
-      top: '-2px',
+      top: 12,
+      left: '50%',
       cursor: 'pointer',
       zIndex: 10,
-      transform: `scale(${ (isHoveringOverAddButton || showEditorSelector) ? 1 : 0.8 })`,
-      transition: 'all 0.15s ease-out'
+      transform: `translateX(-50%) translateY(-50%) scale(0.5)`,
+      transformOrigin: 'center 25% 0px',
+      transition: 'transform 0.15s ease-out'
     };
 
-    if (hasRoomToRenderOnRight) {
-      addButtonStyle.right = '-45px'
-    } else {
-      addButtonStyle.left = '-45px'
-    }
-
-    const containerStyle = {
-      position: 'absolute',
+    let containerStyle = {
+      boxSizing: 'border-box',
+      position: 'relative',
+      height: ADD_ROW_COLLAPSED,
+      minHeight: ADD_ROW_COLLAPSED,
       pointerEvents: 'none',
+      width: '100%',
       left: 0,
       right: 0,
-      opacity: (isHoveringOverContainer || showEditorSelector) ? 1 : 0,
-      transition: 'opacity 0.15s ease-out',
+      opacity: 0,
+      transition: `opacity 0.15s ease-out, height 0.15s ease-out`,
       zIndex: 100
+    };
+
+    if (isHoveringOverAddButton || showEditorSelector) {
+      //alter styles to expanded view
+      hrStyle = {
+        ...hrStyle,
+        height: ADD_ROW_EXPANDED,
+        border: '2px dashed  #00b850',
+        opacity: 1
+      };
+
+      addButtonStyle = {
+        ...addButtonStyle,
+        top: 8,
+        transform: `translateX(-50%) translateY(-50%) scale(1)`
+      };
+
+      containerStyle = {
+        ...containerStyle,
+        height: ADD_ROW_EXPANDED,
+      };
+
+    }
+
+    if (isHoveringOverContainer || showEditorSelector) {
+      //alter styles whenever a users mouse is over any part of the wysiwyg editor
+      hrStyle = {
+        ...hrStyle,
+        opacity: 0.5
+      };
+
+      containerStyle = {
+        ...containerStyle,
+        opacity: 1
+      };
+    }
+
+
+
+    const wrapperStyle = {
+      boxSizing: 'border-box',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      position: 'relative',
+      zIndex: 10,
     };
 
     return (
       <div className="add-row" style={containerStyle}>
-        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', zIndex: 10 }} ref={(el) => this.wrapper = el}>
+        <div style={wrapperStyle} ref={(el) => this.wrapper = el}>
           <div
             style={addButtonStyle}
-            onMouseOver={() => this.setState({ isHoveringOverAddButton: true })}
-            onMouseOut={() => this.setState({ isHoveringOverAddButton: false })}
+            onMouseOver={() => !isHoveringOverAddButton && this.setState({ isHoveringOverAddButton: true })}
+            onMouseOut={() => isHoveringOverAddButton && this.setState({ isHoveringOverAddButton: false })}
             ref={(el) => this.addButton = el}
           >
             <AddButtonContainer
@@ -103,11 +154,14 @@ export default class AddButtonHorizRule extends React.Component {
               onClick={ this.handleAddNew }
               showEditorSelector={ showEditorSelector }
               internalAllowedEditorTypes={ internalAllowedEditorTypes }
+              inverColor={ !isHoveringOverAddButton }
               shadow={false}
             />
 
           </div>
-          <hr style={hrStyle} />
+          <section style={hrStyle}
+            onMouseOver={() => !isHoveringOverAddButton && this.setState({ isHoveringOverAddButton: true })}
+            onMouseOut={() => isHoveringOverAddButton && this.setState({ isHoveringOverAddButton: false })}/>
         </div>
       </div>
     );
